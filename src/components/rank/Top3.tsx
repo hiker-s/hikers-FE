f45da56fdb35aa40dd04edd7eb26776d34836979
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-04-18 18:33:39 +0900

    Chore: 더미데이터 변경

--- src/components/rank/Top3.tsx
+++ src/components/rank/Top3.tsx
@@ -5,27 +5,22 @@ import crown from "../../assets/images/crown.svg";
 
 const Top3 = () => {
   const dummyRank = {
-    top3: [
-      {
-        id: 1,
-        name: "하이커스",
-        img: "https://picsum.photos/200/300",
-        stamp: 20,
-      },
-      {
-        id: 2,
-        name: "나는2등이야",
-        img: "",
-        stamp: 15,
-      },
-      {
-        id: 3,
-        name: "삼등이",
-        img: "https://picsum.photos/200/300",
-        stamp: 10,
-      },
+    myRank: { id: 23, rank: 23, name: "하이커스", img: "", stamp: 11, status: "same" },
+    ranker: [
+      { id: 1, rank: 1, name: "하이하이", img: "", stamp: 10, status: "same" },
+      { id: 2, rank: 2, name: "안녕", img: "https://picsum.photos/200/300", stamp: 42, status: "down" },
+      { id: 3, rank: 3, name: "하이커", img: "", stamp: 38, status: "same" },
+      { id: 4, rank: 4, name: "이커스", img: "https://picsum.photos/200/300", stamp: 35, status: "up" },
+      { id: 5, rank: 5, name: "하아이", img: "https://picsum.photos/200/300", stamp: 32, status: "down" },
+      { id: 6, rank: 6, name: "커어스", img: "", stamp: 31, status: "up" },
+      { id: 7, rank: 7, name: "커어스", img: "https://picsum.photos/200/300", stamp: 31, status: "up" },
+      { id: 8, rank: 8, name: "커어스", img: "https://picsum.photos/200/300", stamp: 31, status: "up" },
+      { id: 9, rank: 9, name: "커어스", img: "https://picsum.photos/200/300", stamp: 31, status: "same" },
+      { id: 10, rank: 10, name: "커어스", img: "https://picsum.photos/200/300", stamp: 31, status: "down" },
     ],
   };
+
+  const top3Rankers = dummyRank.ranker.slice(0, 3);
 
   const getContainerComponent = (index: number) => {
     const containers = {
@@ -37,7 +32,7 @@ const Top3 = () => {
   };
 
   // 원하는 순서로 배열을 재정렬
-  const orderedRanks = [dummyRank.top3[1], dummyRank.top3[0], dummyRank.top3[2]];
+  const orderedRanks = [top3Rankers[1], top3Rankers[0], top3Rankers[2]];
 
   return (
     <Styled.Wrapper $backgroundImage={top3Background}>
