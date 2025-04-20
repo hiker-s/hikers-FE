72a3da385335d16008cc6f4d4f653d03e5b79aa0
ynzung <syj91105@sookmyung.ac.kr>
2025-04-20 15:55:39 +0900

    Feat: 커뮤니티 페이지 내 컴포넌트 import

--- src/pages/community/Community.styled.ts
+++ src/pages/community/Community.styled.ts
@@ -5,12 +5,3 @@ export const Wrapper = styled.div`
   flex-direction: column;
   gap: 3.75rem;
 `;
-
-export const ListCompWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  gap: 0.3125rem;
-`;
-
-export const CarouselWrapper = styled.div``;
