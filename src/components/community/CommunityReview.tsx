30f9e6131ba00ef9ac48b4cfc0f08b3556904611
ynzung <syj91105@sookmyung.ac.kr>
2025-04-20 17:31:20 +0900

    Refactor: 커뮤니티 페이지 리뷰 컴포넌트 데이터 재선언

--- src/components/community/CommunityReview.tsx
+++ src/components/community/CommunityReview.tsx
@@ -3,9 +3,39 @@ import { GreenBtn } from "../common/button/styled";
 import ReviewList from "../review/ReviewList";
 
 export default function CommunityReview() {
+  const MOCK_COMMUNITY_REVIEW = [
+    {
+      id: 1,
+      title: "인왕산 껌이네",
+      mountainName: "인왕산",
+      courseName: "인왕산1코스인왕산1코스인왕산1코스",
+      level: "상",
+      isLiked: true,
+      isWriter: false,
+    },
+    {
+      id: 2,
+      title: "북한산 껌이네",
+      mountainName: "북한산",
+      courseName: "북한산3코스",
+      level: "중",
+      isLiked: false,
+      isWriter: true,
+    },
+    {
+      id: 3,
+      title: "관악산 껌이네",
+      mountainName: "관악산",
+      courseName: "관악산5코스",
+      level: "하",
+      isLiked: true,
+      isWriter: true,
+    },
+  ];
+
   return (
     <Styled.Wrapper>
-      <ReviewList title="리뷰" />
+      <ReviewList title="리뷰" data={MOCK_COMMUNITY_REVIEW} />
       <Styled.ButtonWrapper>
         <GreenBtn
           onClick={() => console.log("리뷰 작성 버튼 클릭")}
