5ad9b812a4463902cd858dfb546b013cd3bab4ef
ynzung <syj91105@sookmyung.ac.kr>
2025-04-22 20:50:32 +0900

    Feat: 마이페이지 UI 구현

--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -1,7 +1,12 @@
+import * as Styled from "./styled";
 import { useNavigate } from "react-router-dom";
 import { Header } from "../../components/common/header/Header";
 import { Layout } from "../../components/common/layout/Layout";
 import UserInfo from "../../components/mypage/UserInfo";
+import Stamp from "../../components/mypage/Stamp";
+import ScrapedList from "../../components/mypage/ScrapedList";
+import LikedList from "../../components/mypage/likedList";
+import MyReviewList from "../../components/mypage/MyReviewList";
 
 export default function Mypage() {
   const navigate = useNavigate();
@@ -14,6 +19,12 @@ export default function Mypage() {
       <Layout $margin="6.25rem 0 1rem 0" isFooter={true}>
         <Header onClick={handleBackBtn}>마이페이지</Header>
         <UserInfo />
+        <Stamp />
+        <Styled.ListWrapper>
+          <ScrapedList />
+          <MyReviewList />
+          <LikedList />
+        </Styled.ListWrapper>
       </Layout>
     </div>
   );
