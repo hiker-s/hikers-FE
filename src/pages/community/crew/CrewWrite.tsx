5d08319a1217032eb1230bdd63bd6251d2cf641b
ynzung <syj91105@sookmyung.ac.kr>
2025-04-23 02:56:16 +0900

    Feat: 크루 모집 글 페이지 UI 구현

---
+++ src/pages/community/crew/CrewWrite.tsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { Header } from "../../../components/common/header/Header";
+import { Layout } from "../../../components/common/layout/Layout";
+import CrewForm from "../../../components/community/crew/CrewForm";
+
+export default function CrewWrite() {
+  const navigate = useNavigate();
+
+  const handleBackBtn = () => {
+    navigate("/community");
+  };
+
+  const today = new Date();
+  const date_info = `${today.getFullYear()}.${(today.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}.${today.getDate().toString().padStart(2, "0")}`;
+
+  const nickname = "하이커스";
+  return (
+    <Layout $margin="6.81rem 0 1rem 0" isFooter={true}>
+      <Header onClick={handleBackBtn}>크루</Header>
+      <CrewForm date_info={date_info} nickname={nickname} />
+    </Layout>
+  );
+}
