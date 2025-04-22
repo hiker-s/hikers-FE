e5bd581b46b4fbc4000bfc09ada6cb7320724f2a
ynzung <syj91105@sookmyung.ac.kr>
2025-04-22 20:05:15 +0900

    Chore: 커뮤니티 내 리뷰 컴포넌트 변수명 변경

--- src/components/community/CommunityReview.tsx
+++ src/components/community/CommunityReview.tsx
@@ -5,37 +5,40 @@ import ReviewList from "../common/list/ReviewList";
 export default function CommunityReview() {
   const MOCK_COMMUNITY_REVIEW = [
     {
-      id: 1,
+      review_id: 1,
       title: "인왕산 껌이네",
-      mountainName: "인왕산",
-      courseName: "인왕산1코스인왕산1코스인왕산1코스",
+      mnt_name: "인왕산",
+      course_name: "인왕산1코스인왕산1코스인왕산1코스",
       level: "상",
-      isLiked: true,
-      isWriter: false,
+      is_liked: true,
+      is_writer: false,
+      like_count: 23,
     },
     {
-      id: 2,
+      review_id: 2,
       title: "북한산 껌이네",
-      mountainName: "북한산",
-      courseName: "북한산3코스",
+      mnt_name: "북한산",
+      course_name: "북한산3코스",
       level: "중",
-      isLiked: false,
-      isWriter: true,
+      is_liked: false,
+      is_writer: true,
+      like_count: 23,
     },
     {
-      id: 3,
+      review_id: 3,
       title: "관악산 껌이네",
-      mountainName: "관악산",
-      courseName: "관악산5코스",
+      mnt_name: "관악산",
+      course_name: "관악산5코스",
       level: "하",
-      isLiked: true,
-      isWriter: true,
+      is_liked: true,
+      is_writer: true,
+      like_count: 23,
     },
   ];
 
   return (
     <Styled.Wrapper>
-      <ReviewList title="리뷰" data={MOCK_COMMUNITY_REVIEW} />
+      <ReviewList title="리뷰" review_data={MOCK_COMMUNITY_REVIEW} />
       <Styled.ButtonWrapper>
         <GreenBtn
           onClick={() => console.log("리뷰 작성 버튼 클릭")}
