27522e468f519e11ad48107992926e25be4c4f93
ynzung <syj91105@sookmyung.ac.kr>
2025-04-22 13:54:07 +0900

    Design: 스탬프 컴포넌트 스타일 설정

---
+++ src/components/mypage/Stamp.styled.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.62rem;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.31rem;
+`;
+
+export const Title = styled.div`
+  color: #3b3b3b;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const StampCount = styled.div`
+  color: #3b3b3b;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+export const StampWrapper = styled.div``;
+
+export const BtnWrapper = styled.div`
+  display: flex;
+  padding: 0.44rem 0;
+  justify-content: flex-end;
+`;
