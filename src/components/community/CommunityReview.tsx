98d9242d668e6108c3608b913d821e388ce2593b
ynzung <syj91105@sookmyung.ac.kr>
2025-04-23 20:54:12 +0900

    Design: 커뮤니티-리뷰작성 버튼 스타일 수정

--- src/components/community/CommunityReview.tsx
+++ src/components/community/CommunityReview.tsx
@@ -1,6 +1,6 @@
 import * as Styled from "./CommunityReview.styled";
-import { GreenBtn } from "../common/button/styled";
 import ReviewList from "../common/list/ReviewList";
+import { GreenBtn } from "../common/button/GreenBtn";
 
 export default function CommunityReview() {
   const MOCK_COMMUNITY_REVIEW = [
@@ -40,16 +40,7 @@ export default function CommunityReview() {
     <Styled.Wrapper>
       <ReviewList title="리뷰" review_data={MOCK_COMMUNITY_REVIEW} />
       <Styled.ButtonWrapper>
-        <GreenBtn
-          onClick={() => console.log("리뷰 작성 버튼 클릭")}
-          $padding="0.38rem 1rem"
-          $bgColor="#349989"
-          color="white"
-          $fontSize="0.875rem"
-          $width="6.75rem"
-        >
-          리뷰 작성하기
-        </GreenBtn>
+        <GreenBtn onClick={() => console.log("리뷰 작성 버튼 클릭")}>리뷰 작성하기</GreenBtn>
       </Styled.ButtonWrapper>
     </Styled.Wrapper>
   );
