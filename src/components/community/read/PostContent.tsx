c48df04fc2bb48d0dd7777a5d342d8c6681f3bf4
ynzung <syj91105@sookmyung.ac.kr>
2025-04-26 02:16:14 +0900

    Chore: 커뮤니티 글 조회 이미지 배열 변수명 수정

--- src/components/community/read/PostContent.tsx
+++ src/components/community/read/PostContent.tsx
@@ -2,22 +2,24 @@ import * as Styled from "./styled";
 
 type PostContentProps = {
   content: string;
-  images: string[];
+  images_url: string[];
 };
 
-export default function PostContent({ content, images }: PostContentProps) {
+export default function PostContent({ content, images_url }: PostContentProps) {
   return (
-    <Styled.ContentWrapper>
-      {images.length > 0 && (
-        <Styled.ImageWrapper>
-          {images.map((image, index) => (
-            <Styled.ImageContent key={index}>
-              <img src={image} alt={`이미지 ${index + 1} 장`} />
-            </Styled.ImageContent>
-          ))}
-        </Styled.ImageWrapper>
-      )}
-      <Styled.Content>{content}</Styled.Content>
-    </Styled.ContentWrapper>
+    <div>
+      <Styled.ContentWrapper>
+        {images_url.length > 0 && (
+          <Styled.ImageWrapper>
+            {images_url.map((image, index) => (
+              <Styled.ImageContent key={index}>
+                <img src={image} alt={`이미지 ${index + 1} 장`} />
+              </Styled.ImageContent>
+            ))}
+          </Styled.ImageWrapper>
+        )}
+        <Styled.Content>{content}</Styled.Content>
+      </Styled.ContentWrapper>
+    </div>
   );
 }
