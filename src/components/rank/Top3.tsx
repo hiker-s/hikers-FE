d77960e76ff2db8f06fb061c4ddc3f77f0c762c6
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-04-27 10:36:08 +0900

    Design: Ranker 이미지 다 default 이미지로 변경

--- src/components/rank/Top3.tsx
+++ src/components/rank/Top3.tsx
@@ -5,18 +5,18 @@ import crown from "../../assets/images/crown.svg";
 
 const Top3 = () => {
   const dummyRank = {
-    myRank: { id: 23, rank: 23, name: "하이커스", img: "", stamp: 11, status: "same" },
+    myRank: { id: 23, rank: 23, name: "하이커스", stamp: 11, status: "same" },
     ranker: [
-      { id: 1, rank: 1, name: "하이하이", img: "", stamp: 10, status: "same" },
-      { id: 2, rank: 2, name: "안녕", img: "https://picsum.photos/200/300", stamp: 42, status: "down" },
-      { id: 3, rank: 3, name: "하이커", img: "", stamp: 38, status: "same" },
-      { id: 4, rank: 4, name: "이커스", img: "https://picsum.photos/200/300", stamp: 35, status: "up" },
-      { id: 5, rank: 5, name: "하아이", img: "https://picsum.photos/200/300", stamp: 32, status: "down" },
-      { id: 6, rank: 6, name: "커어스", img: "", stamp: 31, status: "up" },
-      { id: 7, rank: 7, name: "커어스", img: "https://picsum.photos/200/300", stamp: 31, status: "up" },
-      { id: 8, rank: 8, name: "커어스", img: "https://picsum.photos/200/300", stamp: 31, status: "up" },
-      { id: 9, rank: 9, name: "커어스", img: "https://picsum.photos/200/300", stamp: 31, status: "same" },
-      { id: 10, rank: 10, name: "커어스", img: "https://picsum.photos/200/300", stamp: 31, status: "down" },
+      { id: 1, rank: 1, name: "하이하이", stamp: 10, status: "same" },
+      { id: 2, rank: 2, name: "안녕", stamp: 42, status: "down" },
+      { id: 3, rank: 3, name: "하이커", stamp: 38, status: "same" },
+      { id: 4, rank: 4, name: "이커스", stamp: 35, status: "up" },
+      { id: 5, rank: 5, name: "하아이", stamp: 32, status: "down" },
+      { id: 6, rank: 6, name: "커어스", stamp: 31, status: "up" },
+      { id: 7, rank: 7, name: "커어스", stamp: 31, status: "up" },
+      { id: 8, rank: 8, name: "커어스", stamp: 31, status: "up" },
+      { id: 9, rank: 9, name: "커어스", stamp: 31, status: "same" },
+      { id: 10, rank: 10, name: "커어스", stamp: 31, status: "down" },
     ],
   };
 
@@ -42,7 +42,7 @@ const Top3 = () => {
           return (
             <Container key={rank.id}>
               {rank.id === 1 && <Styled.CrownImg src={crown} />}
-              <Styled.TopImg src={rank.img || top3Default} />
+              <Styled.TopImg src={top3Default} />
               <Styled.TopTextContainer>
                 <Styled.TopName>{rank.name}</Styled.TopName>
                 <Styled.TopStamp>
