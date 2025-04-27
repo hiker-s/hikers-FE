c5238804a88cd92f13cf090ce910b46dc0fc5b1b
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-04-27 09:18:28 +0900

    Retactor: mnt 데이터 타입 선언 파일 추가

---
+++ src/types/mountainData.d.ts
@@ -0,0 +1,31 @@
+declare module "*/data/mnt/*.json" {
+  interface MountainData {
+    mnt_id: number;
+    mnt_name: string;
+    course_id: number;
+    max_ele: number;
+    total_length_km: string;
+    total_time: string;
+    start_name: string;
+    end_name: string;
+    level: string;
+    course_name: string;
+    min_ele: number;
+    track: Array<{
+      path_id: number;
+      path: Array<{
+        lat: number;
+        lng: number;
+        ele: number;
+      }>;
+      color: string;
+      start_name: string;
+      end_name: string;
+      length_meter: number;
+      time_minute: number;
+    }>;
+  }
+
+  const value: MountainData;
+  export default value;
+}
