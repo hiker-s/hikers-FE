65ce9637b4d61ecb90466fc7cd443e04c709549a
ynzung <syj91105@sookmyung.ac.kr>
2025-04-27 17:16:14 +0900

    Del: 안쓰는 코드 삭제

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -46,11 +46,6 @@ const SignUp = () => {
             case 409:
               alert("이미 사용 중인 아이디입니다.");
               break;
-            case 400:
-              alert("입력하신 정보를 다시 확인해주세요.");
-              break;
-            default:
-              alert("회원가입 중 오류가 발생했습니다. 다시 시도해주세요.");
           }
         } else if (error.request) {
           alert("서버와 통신할 수 없습니다. 네트워크 연결을 확인해주세요.");
