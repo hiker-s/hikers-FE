d8b76c72fee10a4a1b7414fbd19eb9b0623b25aa
ynzung <syj91105@sookmyung.ac.kr>
2025-04-28 19:30:55 +0900

    Feat: 커뮤니티 크루 글 상세 조회 페이지 연동 구현

--- src/pages/community/crew/CrewRead.tsx
+++ src/pages/community/crew/CrewRead.tsx
@@ -5,8 +5,9 @@ import { Layout } from "../../../components/common/layout/Layout";
 import PostContent from "../../../components/community/read/PostContent";
 import PostCreateInfo from "../../../components/community/read/PostCreateInfo";
 import PostTitle from "../../../components/community/read/PostTitle";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ButtonGroup from "../../../components/community/read/ButtonGroup";
+import { communityApi, CrewDetail } from "../../../apis/community/CommunityApi";
 
 export default function CrewRead() {
   const navigate = useNavigate();
@@ -15,41 +16,47 @@ export default function CrewRead() {
   };
 
   const { crew_id } = useParams();
-  const post_id = parseInt(crew_id ?? "", 10);
+  const id = parseInt(crew_id ?? "", 10);
 
-  const MOCK_CREW_DETAIL = [
-    {
-      title: "크루 모집 제목입니다.",
-      created_at: "2025.04.01",
-      author_name: "하이커스",
-      is_writer: true,
-      content: "크루 모집 내용입니다.",
-      image_urls: [
-        "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDEwMjBfMjky%2FMDAxNzI5NDA1OTcwNzEz.vgbx7hvDEulsx48OE4T8wwTH_bnAX5CNBU_Y2WiEkfgg.D3M27ocuqJld1BaoXaurFg8JuoC3F1vD9o2vxjY6w0sg.JPEG%2FKakaoTalk_20241020_151047702_15.jpg&type=sc960_832",
-        "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20130916_18%2Fjisan80_137929009080279rQi_JPEG%2F1-%25BA%25D2%25BE%25CF2-1300.jpg&type=sc960_832",
-      ],
-    },
-  ];
-  const [crewDetailData] = useState(MOCK_CREW_DETAIL);
+  const [crewDetailData, setCrewDetailData] = useState<CrewDetail>();
+
+  useEffect(() => {
+    const fetchCrewDetail = async (id: number) => {
+      try {
+        const crew = await communityApi.getCrewDetail(id);
+        setCrewDetailData(crew);
+      } catch (error) {
+        console.error("크루 글 상세 데이터 가져오기 실패:", error);
+      }
+    };
+
+    if (!isNaN(id)) {
+      fetchCrewDetail(id);
+    }
+  }, [id]);
 
   return (
     <Layout $margin="6.81rem 0 0 0" $isFooter={true}>
       <Header onClick={handleBackBtn}>크루</Header>
       <Wrapper>
-        <PostTitle title={crewDetailData[0].title} />
-        <PostCreateInfo
-          is_review={false}
-          created_at={crewDetailData[0].created_at}
-          author_name={crewDetailData[0].author_name}
-          is_writer={crewDetailData[0].is_writer}
-        />
-        <PostContent content={crewDetailData[0].content} image_urls={crewDetailData[0].image_urls} />
+        {crewDetailData && (
+          <>
+            <PostTitle title={crewDetailData.title} />
+            <PostCreateInfo
+              is_review={false}
+              created_at={crewDetailData.created_at}
+              author_name={crewDetailData.author_name}
+              is_writer={crewDetailData.is_writer}
+            />
+            <PostContent content={crewDetailData.content} image_urls={crewDetailData.image_urls} />
+            {crewDetailData.is_writer && (
+              <ButtonWrapper>
+                <ButtonGroup mode="crew" id={id} />
+              </ButtonWrapper>
+            )}
+          </>
+        )}
       </Wrapper>
-      {crewDetailData[0].is_writer && (
-        <ButtonWrapper>
-          <ButtonGroup mode="crew" id={post_id} />
-        </ButtonWrapper>
-      )}
     </Layout>
   );
 }
