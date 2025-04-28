c0df985992d5414b2bf35d9190c308d3104805ee
ynzung <syj91105@sookmyung.ac.kr>
2025-04-28 23:36:59 +0900

    Feat: 커뮤니티 리뷰 좋아요, 좋아요 취소 api 연동 코드 작성

--- src/components/community/CommunityReview.tsx
+++ src/components/community/CommunityReview.tsx
@@ -1,44 +1,49 @@
 import * as Styled from "./CommunityReview.styled";
 import ReviewList from "../common/list/ReviewList";
 import { GreenBtn } from "../common/button/GreenBtn";
+import { useEffect, useState } from "react";
+import { reviewApi, ReviewListAPI } from "../../apis/community/ReviewApi";
 
 export default function CommunityReview() {
-  const MOCK_COMMUNITY_REVIEW = [
-    {
-      id: 1,
-      title: "인왕산 껌이네",
-      mountain_name: "인왕산",
-      course_name: "인왕산1코스인왕산1코스인왕산1코스",
-      level: "상",
-      liked_by_current_user: true,
-      is_writer: false,
-      like_count: 23,
-    },
-    {
-      id: 2,
-      title: "북한산 껌이네",
-      mountain_name: "북한산",
-      course_name: "북한산3코스",
-      level: "중",
-      liked_by_current_user: false,
-      is_writer: true,
-      like_count: 23,
-    },
-    {
-      id: 3,
-      title: "관악산 껌이네",
-      mountain_name: "관악산",
-      course_name: "관악산5코스",
-      level: "하",
-      liked_by_current_user: true,
-      is_writer: true,
-      like_count: 23,
-    },
-  ];
+  const [reviewData, setReviewData] = useState<ReviewListAPI[]>([]);
+
+  useEffect(() => {
+    const fetchCrewList = async () => {
+      try {
+        const crew = await reviewApi.getReviewList();
+        setReviewData(crew);
+      } catch (error) {
+        console.error("리뷰 글 목록 데이터 가져오기 실패:", error);
+      }
+    };
+
+    fetchCrewList();
+  }, []);
+
+  const handleLikeToggle = async (itemId: number) => {
+    setReviewData((prevData) =>
+      prevData.map((item) =>
+        item.id === itemId ? { ...item, liked_by_current_user: !item.liked_by_current_user } : item
+      )
+    );
+    const currentItem = reviewData.find((item) => item.id === itemId);
+
+    if (!currentItem) return;
+
+    try {
+      if (currentItem.liked_by_current_user) {
+        await reviewApi.deleteReviewHeart(itemId);
+      } else {
+        await reviewApi.postReviewHeart(itemId);
+      }
+    } catch (error) {
+      console.error("좋아요 토글 실패:", error);
+    }
+  };
 
   return (
     <Styled.Wrapper>
-      <ReviewList title="리뷰" review_data={MOCK_COMMUNITY_REVIEW} />
+      <ReviewList title="리뷰" review_data={reviewData} onLikeToggle={handleLikeToggle} />
       <Styled.ButtonWrapper>
         <GreenBtn onClick={() => console.log("리뷰 작성 버튼 클릭")}>리뷰 작성하기</GreenBtn>
       </Styled.ButtonWrapper>
