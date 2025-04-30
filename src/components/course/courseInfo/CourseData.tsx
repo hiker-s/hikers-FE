aa2523d0ebe3b3009d6e911eed80aa0118f26d0f
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-04-30 22:58:46 +0900

    CourseData json 연동중 ...

--- src/components/course/courseInfo/CourseData.tsx
+++ src/components/course/courseInfo/CourseData.tsx
@@ -1,37 +1,27 @@
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 import { Modal } from "../../common/modal/Modal";
 import { GreenBtn } from "../../common/button/GreenBtn";
 import { LevelComp } from "../../common/item/Level";
 import * as Styled from "./CourseData.styled";
-import mntData from "../../../data/mnt/가리산/가리산_1.json";
 import useKakaoShare from "../../../hooks/useKakaoShare";
-
-// 타입 가드 함수 - json 형식
-function isMountainData(data: unknown): data is typeof mntData {
-  const d = data as typeof mntData;
-  return (
-    d !== null &&
-    typeof d === "object" &&
-    typeof d.course_name === "string" &&
-    typeof d.mnt_name === "string" &&
-    typeof d.total_length_km === "string" &&
-    typeof d.max_ele === "number" &&
-    typeof d.total_time === "string" &&
-    typeof d.start_name === "string" &&
-    typeof d.end_name === "string" &&
-    typeof d.level === "string"
-  );
-}
+// import Skeleton from "react-loading-skeleton";
+// import "react-loading-skeleton/dist/skeleton.css";
+import course_ids from "../../../data/course_ids.json";
+import { MountainData } from "../../../types/mountainData";
 
 const CourseData = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { shareCourse } = useKakaoShare();
 
-  // 타입 검사
-  if (!isMountainData(mntData)) {
-    console.error("Invalid mountain data structure:", mntData);
-    return <div>데이터 형식이 올바르지 않습니다.</div>;
-  }
+  const { course_id } = useParams();
+  const id = parseInt(course_id ?? "", 10);
+
+  const mnt_courseId = course_ids.find((item) => Number(item.course_id) === id);
+
+  const mntData = import.meta.glob<MountainData>(`/src/data/mnt/${mnt_courseId?.courseFilePath}`, {
+    eager: true,
+  });
 
   const totalDistance = mntData.total_length_km;
   const totalElevation = `${mntData.max_ele}m`;
@@ -71,6 +61,29 @@ const CourseData = () => {
         />
       )}
       <Styled.Wrapper>
+        <Styled.CourseTitleWrapper>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Styled.CourseTitle>{courseTitle}</Styled.CourseTitle>
+            <LevelComp $level={courseLevel}>{courseLevel}</LevelComp>
+          </div>
+          <Styled.GreenBtnWrapper>
+            <GreenBtn onClick={() => setIsModalOpen(true)}>코스 공유하기</GreenBtn>
+          </Styled.GreenBtnWrapper>
+        </Styled.CourseTitleWrapper>
+        <Styled.CourseMeta>
+          <Styled.StartToEnd>{courseStartEnd}</Styled.StartToEnd>
+          <Styled.CourseStats>
+            <Styled.CourseStatsItem>
+              <span style={{ color: "#A4A4A4" }}>소요시간</span> {totalDuration}
+            </Styled.CourseStatsItem>
+            <Styled.CourseStatsItem>
+              <span style={{ color: "#A4A4A4" }}>코스길이</span> {totalDistance}
+            </Styled.CourseStatsItem>
+            <Styled.CourseStatsItem>
+              <span style={{ color: "#A4A4A4" }}>고도</span> {totalElevation}
+            </Styled.CourseStatsItem>
+          </Styled.CourseStats>
+        </Styled.CourseMeta>
         <Styled.CourseTitleWrapper>
           <div style={{ display: "flex", alignItems: "center" }}>
             <Styled.CourseTitle>{courseTitle}</Styled.CourseTitle>
