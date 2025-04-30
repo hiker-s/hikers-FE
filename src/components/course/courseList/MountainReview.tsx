184eded6f2e260f813a78884a247ac430c1c22af
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-04-30 19:25:36 +0900

    Feat: 산 리뷰 조회 api 연동

--- src/components/course/courseList/MountainReview.tsx
+++ src/components/course/courseList/MountainReview.tsx
@@ -1,36 +1,43 @@
 import { CardList } from "../../common/card/CardList";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { mntReviewApi, MntReviewItem } from "../../../apis/course/courseList/MountainReviewApi";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const CourseReview = () => {
-  const dummyCourseReview = [
-    {
-      id: 1,
-      title: "나들이로 딱 !",
-      level: "중",
-      description: "쉬운 난이도였지만 운동 안해서 등산이라 힘들었다",
-      imgUrl: "https://picsum.photos/200/300",
-      isLiked: false,
-    },
-    {
-      id: 2,
-      title: "나들이로 딱 !",
-      level: "하",
-      description: "아주아주 쉬웠어 최고 짱짱이야 ~~ 추천 합니다",
-      imgUrl: "",
-      isLiked: true,
-    },
-    {
-      id: 3,
-      title: "나들이로 딱 !",
-      level: "상",
-      description: "쉬운 난이도였지만 운동 안해서 등산이라 힘들었다",
-      imgUrl: "",
-      isLiked: true,
-    },
-  ];
+  const [mntReview, setMntReview] = useState<MntReviewItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState("최신순");
+
+  const { mnt_id } = useParams();
+  const id = parseInt(mnt_id ?? "", 10);
+
+  useEffect(() => {
+    if (isNaN(id)) return;
+
+    const fetchMntReview = async () => {
+      try {
+        setIsLoading(true);
+        const data = await mntReviewApi.getMntReview(id, type);
+        setMntReview(data);
+      } catch (error) {
+        console.error("산 리뷰 가져오기 실패:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMntReview();
+  }, [id, type]);
 
   return (
     <>
-      <CardList items={dummyCourseReview} />
+      {isLoading ? (
+        <Skeleton width={"100%"} height={"11.100%"} />
+      ) : (
+        <CardList items={mntReview} type={type} onTypeChange={setType} />
+      )}
     </>
   );
 };
