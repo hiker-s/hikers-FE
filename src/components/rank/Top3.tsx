907c48bbe8960e94ee8abfda74efafbc15192f74
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-05-01 03:01:49 +0900

    Feat: Top3 api 연동

--- src/components/rank/Top3.tsx
+++ src/components/rank/Top3.tsx
@@ -2,25 +2,33 @@ import * as Styled from "./Top3.styled";
 import top3Background from "../../assets/images/top3Background.svg";
 import top3Default from "../../assets/images/top3Default.svg";
 import crown from "../../assets/images/crown.svg";
+import { rankApi, Rankers } from "../../apis/rank/RankApi";
+import { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
 
 const Top3 = () => {
-  const dummyRank = {
-    myRank: { id: 23, rank: 23, name: "하이커스", stamp: 11, status: "same" },
-    ranker: [
-      { id: 1, rank: 1, name: "하이하이", stamp: 10, status: "same" },
-      { id: 2, rank: 2, name: "안녕", stamp: 42, status: "down" },
-      { id: 3, rank: 3, name: "하이커", stamp: 38, status: "same" },
-      { id: 4, rank: 4, name: "이커스", stamp: 35, status: "up" },
-      { id: 5, rank: 5, name: "하아이", stamp: 32, status: "down" },
-      { id: 6, rank: 6, name: "커어스", stamp: 31, status: "up" },
-      { id: 7, rank: 7, name: "커어스", stamp: 31, status: "up" },
-      { id: 8, rank: 8, name: "커어스", stamp: 31, status: "up" },
-      { id: 9, rank: 9, name: "커어스", stamp: 31, status: "same" },
-      { id: 10, rank: 10, name: "커어스", stamp: 31, status: "down" },
-    ],
-  };
+  const [rankers, setRankers] = useState<Rankers["result"] | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const top3Rankers = dummyRank.ranker.slice(0, 3);
+  useEffect(() => {
+    const fetchRankers = async () => {
+      try {
+        setIsLoading(true);
+        const data = await rankApi.getRankers();
+        setRankers(data);
+
+        console.log(data);
+      } catch (error) {
+        console.error("산 리뷰 가져오기 실패:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchRankers();
+  }, []);
+
+  const top3Rankers = rankers?.ranker.slice(0, 3) ?? [];
 
   const getContainerComponent = (index: number) => {
     const containers = {
@@ -32,28 +40,38 @@ const Top3 = () => {
   };
 
   // 원하는 순서로 배열을 재정렬
-  const orderedRanks = [top3Rankers[1], top3Rankers[0], top3Rankers[2]];
+  const orderedRanks = top3Rankers.length === 3 ? [top3Rankers[1], top3Rankers[0], top3Rankers[2]] : top3Rankers;
 
   return (
-    <Styled.Wrapper $backgroundImage={top3Background}>
-      <Styled.TopContainer>
-        {orderedRanks.map((rank, index) => {
-          const Container = getContainerComponent(index);
-          return (
-            <Container key={rank.id}>
-              {rank.id === 1 && <Styled.CrownImg src={crown} />}
-              <Styled.TopImg src={top3Default} />
-              <Styled.TopTextContainer>
-                <Styled.TopName>{rank.name}</Styled.TopName>
-                <Styled.TopStamp>
-                  <span style={{ fontWeight: "700" }}>{rank.stamp}</span> stamps
-                </Styled.TopStamp>
-              </Styled.TopTextContainer>
-            </Container>
-          );
-        })}
-      </Styled.TopContainer>
-    </Styled.Wrapper>
+    <>
+      {isLoading ? (
+        <>
+          <Styled.Wrapper $backgroundImage={top3Background}>
+            <Skeleton width={"100%"} height={"100%"} />
+          </Styled.Wrapper>
+        </>
+      ) : (
+        <Styled.Wrapper $backgroundImage={top3Background}>
+          <Styled.TopContainer>
+            {orderedRanks.map((rank, index) => {
+              const Container = getContainerComponent(index);
+              return (
+                <Container key={rank.id}>
+                  {index === 1 && <Styled.CrownImg src={crown} />}
+                  <Styled.TopImg src={top3Default} />
+                  <Styled.TopTextContainer>
+                    <Styled.TopName>{rank.name}</Styled.TopName>
+                    <Styled.TopStamp>
+                      <span style={{ fontWeight: "700" }}>{rank.stampCount}</span> stamps
+                    </Styled.TopStamp>
+                  </Styled.TopTextContainer>
+                </Container>
+              );
+            })}
+          </Styled.TopContainer>
+        </Styled.Wrapper>
+      )}
+    </>
   );
 };
 
