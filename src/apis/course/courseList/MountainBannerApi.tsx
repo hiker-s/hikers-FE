88d72b4c50c945fe9d4e059d0094eb396f42f76f
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-04-30 19:08:41 +0900

    Comment: 산 배너 조회 결과 콘솔 로그 주석 처리

--- src/apis/course/courseList/MountainBannerApi.tsx
+++ src/apis/course/courseList/MountainBannerApi.tsx
@@ -12,7 +12,7 @@ export type MntBannerItem = {
 export const mntBannerApi = {
   getMntBanner: async (mnt_id: number): Promise<MntBannerItem> => {
     const response = await axios.get(`${baseURL}/api/mountain/${mnt_id}`);
-    console.log("산 배너 조회 결과:", response.data);
+    // console.log("산 배너 조회 결과:", response.data);
     return response.data.result;
   },
 };
