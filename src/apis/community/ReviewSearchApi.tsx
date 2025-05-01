83146827604d029816681c85158100beb918e0cf
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-05-02 01:03:47 +0900

    Feat: 리뷰 페이지 - 산/코스 전체 조회 api

---
+++ src/apis/community/ReviewSearchApi.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_APP_BASE_URL;
+
+export interface Course {
+  id: number;
+  mountain_id: number;
+  course_file_path: string;
+  course_name: string;
+}
+
+export interface Mountain {
+  id: number;
+  mnt_name: string;
+  view_count: number;
+  courses: Course[];
+}
+
+export interface MntCourseResponse {
+  stats: number;
+  message: string;
+  result: Mountain[];
+}
+
+export const reviewSearchApi = {
+  getMnt_course: async (): Promise<MntCourseResponse> => {
+    const response = await axios.get(`${baseURL}/api/mountain/list`);
+    console.log("산/코스 전체 조회 결과:", response.data);
+    return response.data.result;
+  },
+};
