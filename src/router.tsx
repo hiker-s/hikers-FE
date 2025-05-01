a1edd71ed8c801721d8c648fca137b25773f7f46
ynzung <syj91105@sookmyung.ac.kr>
2025-05-02 02:09:40 +0900

    Feat: not found 페이지 라우터 설정

--- src/router.tsx
+++ src/router.tsx
@@ -16,12 +16,14 @@ import ReviewRead from "./pages/community/review/ReviewRead";
 import Home from "./pages/home/Home";
 import CrewEdit from "./pages/community/crew/CrewEdit";
 import Loading from "./pages/loading/Loading";
+import NotFound from "./pages/error/NotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      { path: "*", element: <NotFound /> },
       { path: "/", element: <Onboarding /> },
       { path: "home", element: <Home /> },
       { path: "login", element: <Login /> },
