7d275c607167bdd08d0d5bb7fe54e4a34f6947c3
ynzung <syj91105@sookmyung.ac.kr>
2025-05-02 15:27:20 +0900

    Fix: 산 배너 높이 고정 스타일 오류 수정

--- src/components/course/courseList/MountainBanner.styled.ts
+++ src/components/course/courseList/MountainBanner.styled.ts
@@ -1,43 +1,27 @@
 import styled, { css } from "styled-components";
 
-export const MountainBannerWrapper = styled.div`
+export const MountainBannerWrapper = styled.div<{ $image?: string }>`
   width: 100%;
-  height: 11.875rem;
+  /* min-height: 11.875rem; */
   flex-shrink: 0;
-  margin-bottom: 1.2rem;
-`;
+  margin-bottom: 1.81rem;
 
-export const BackgroundImage = styled.div<{ $image: string }>`
-  width: 100%;
-  height: 11.875rem;
-  position: relative;
   ${({ $image }) =>
     $image &&
     css`
-      background-image: url(${$image});
+      background: linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), url(${$image});
       background-size: cover;
       background-position: center;
       background-repeat: no-repeat;
-
-      &::before {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: #ffffffb2 70%;
-      }
     `}
-`;
 
-export const TextWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: flex-end;
+  padding: 4rem 0 1.62rem 1.25rem;
+  box-sizing: border-box;
+  background-color: #ffffffb2;
   gap: 1rem;
-  padding: 3.13rem 0 1.25rem 1.25rem;
-  position: absolute;
-  top: 0;
 `;
 
 export const Title = styled.h1`
